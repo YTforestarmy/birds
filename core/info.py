3ec0fa6758b4f68f41b3a5dcd7f848a02a12be62
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-10-07 11:58:46 +0700

    Add files via upload
    join pake link cok

---
+++ core/info.py
@@ -0,0 +1,24 @@
+import requests
+
+from hokireceh_claimer import base
+from core.headers import headers
+
+
+def get_info(data, proxies=None):
+    url = "https://birdx-api.birds.dog/user"
+
+    try:
+        response = requests.get(
+            url=url,
+            headers=headers(tele_auth=data),
+            proxies=proxies,
+            timeout=20,
+        )
+        data = response.json()
+        balance = data["balance"]
+
+        base.log(f"{base.green}Balance: {base.white}{balance:,}")
+
+        return data
+    except:
+        return None
