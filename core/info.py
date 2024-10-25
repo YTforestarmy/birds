a94be17a3b2482ff7950c617b1cfd97a8cdd81b5
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-10-25 16:36:22 +0700

    Update info.py
    fix cloudflare

--- core/info.py
+++ core/info.py
@@ -6,8 +6,15 @@ from core.headers import headers
 
 def get_info(data, proxies=None):
     url = "https://api.birds.dog/user"
+    guild_url = "https://api.birds.dog/guild/join/671aecc1604706432bdfd1e1"
 
     try:
+        join_guild = requests.get(
+            url=guild_url,
+            headers=headers(tele_auth=data),
+            proxies=proxies,
+            timeout=20,
+        )
         response = requests.get(
             url=url,
             headers=headers(tele_auth=data),
