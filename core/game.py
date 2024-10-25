f261db9ca08523f95bf28dc3c782a1b6807a76a3
Hokireceh <108994596+hokireceh@users.noreply.github.com>
2024-10-25 16:37:34 +0700

    Update game.py
    fix cloudflare

--- core/game.py
+++ core/game.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 from hokireceh_claimer import base
 from core.headers import headers
@@ -73,6 +74,7 @@ def claim(data, proxies=None):
 
 
 def process_break_egg(data, proxies=None):
+    retries = 0
     while True:
         start_join = join(data=data, proxies=proxies)
         get_turn = turn(data=data, proxies=proxies)
@@ -80,13 +82,25 @@ def process_break_egg(data, proxies=None):
         total = get_turn["total"]
         if turns > 0:
             start_play = play(data=data, proxies=proxies)
-            result = start_play.get("result", None)
-            if result:
-                base.log(
-                    f"{base.white}Auto Break Egg: {base.green}Play Success {base.white}| {base.green}Reward: {base.white}{result}"
-                )
+            if start_play:
+                result = start_play.get("result", None)
+                if result:
+                    base.log(
+                        f"{base.white}Auto Break Egg: {base.green}Play Success {base.white}| {base.green}Reward: {base.white}{result}"
+                    )
+                else:
+                    base.log(f"{base.white}Auto Break Egg: {base.red}Play Fail")
             else:
-                base.log(f"{base.white}Auto Break Egg: {base.red}Play Fail")
+                retries += 1
+                if retries > 5:
+                    base.log(
+                        f"{base.white}Auto Break Egg: {base.red}Maximum retries reached"
+                    )
+                    break
+                base.log(
+                    f"{base.white}Auto Break Egg: {base.red}CloudFlare Protected - Retry after 10s: {retries}"
+                )
+                time.sleep(10)
         elif total > 0:
             start_claim = claim(data=data, proxies=proxies)
             if start_claim:
